452d8e0ea158be2b9ed13ae4fd3d6adb223b0f85
Fadhiel <90543478+godwimp@users.noreply.github.com>
2025-02-24 11:56:20 +0700

    Delete database-test.php

--- database-test.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $conn = new PDO("mysql:host=localhost;dbname=aduanapp", "root", "");
-    echo "Koneksi berhasil!";
-} catch(PDOException $e) {
-    echo "Koneksi gagal: " . $e->getMessage();
-}
-?>
